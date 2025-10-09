a14e9ba8f7dd1428ded8126354e6a4d67d1f0999
강진아 <Kangjina4006@gmail.com>
2025-10-10 01:09:00 +0900

    2nd Assignment by 202503891 (Kangjina) - Add practice1

--- HUFS-LAI-OOP-2025-2-main/submissions/202401193/assignment2/problem4/dsops/__init__.py
+++
@@ -1,4 +0,0 @@
-from .split.train_test import train_test_split
-from .stats.labels import label_distribution
-
-__all__ = ["train_test_split", "label_distribution"]
--- HUFS-LAI-OOP-2025-2-main/submissions/202401193/assignment2/problem4/dsops/stats/labels.py
+++
@@ -1,4 +0,0 @@
-from collections import Counter
-
-def label_distribution(labels: list[str]) -> dict[str, int]:
-    return dict(Counter(labels))
